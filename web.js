f88b73937b365399a7566a754afff0e4966cfeb7
4sight <hgd4th@gmail.com>
2018-12-28 19:08:59 -0500

    added port variable to web.js

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
+var port=Number(process.env.PORT || 3000);
 var gzippo = require('gzippo');
     var express = require('express');
     var app = express();
     
     app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-    app.listen(process.env.PORT || 5000);
+    app.listen(process.env.PORT || 3000);
