0ae7c7999709ba691655c66e6312e0c27f12ed83
4sight <hgd4th@gmail.com>
2018-12-28 19:23:40 -0500

    changed web.js directory from dist to app

--- web.js
+++ web.js
@@ -3,6 +3,5 @@ var gzippo = require('gzippo');
     var express = require('express');
     var app = express();
     
-    // app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-    app.use(express.static("" + __dirname + "/dist"));
+    app.use(gzippo.staticGzip("" + __dirname + "/app"));
     app.listen(process.env.PORT || 3000);
