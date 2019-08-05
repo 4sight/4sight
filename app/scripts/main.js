8ca36dd63f05b09893877861278355d730de64a5
4sight <hgd4th@gmail.com>
2019-08-05 11:30:40 -0400

    disappearing experience divs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,20 @@ $('.experience').mouseenter(function(){
 	$(this).addClass('linkHover');
 });
 $('.experience').mouseleave(function(){
-  $(this).removeClass('linkHover');
+	$(this).removeClass('linkHover');
+});
+$(window).scroll(function(){
+	if($(window).scrollTop() > 300){
+		$('.experience').addClass('show');
+	} else {
+		$('.experience').removeClass('show');
+	}
 });
 $('a.resume').mouseenter(function(){
 	$(this).addClass('linkHover');
 });
 $('a.resume').mouseleave(function(){
-  $(this).removeClass('linkHover');
+	$(this).removeClass('linkHover');
 });
 $('a#github').hover(function(){
 	$('#githubText').filter(':not(:animated)').animate({
@@ -46,10 +53,6 @@ $('a#soundcloud').hover(function(){
 			opacity: 0
 		})
 	});
-// $('a#phone').click(function(){
-// 	$('#phoneText').filter(':not(:animated)').animate({
-// 		opacity: 1
-// 	})};
 $('a#phone').hover(function(){
 	$('#phoneText').filter(':not(:animated)').animate({
 		opacity: 1
